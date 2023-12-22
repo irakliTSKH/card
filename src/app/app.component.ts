680853da9bed098410f9b097cc9eadad474a02c8
irakliTSKH <tskhelishvili.irakli@gmail.com>
2023-12-23 02:39:03 +0400

    create card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'socar-card2';
+  days = ['ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი','შაბათი', 'კვრია']
 }
